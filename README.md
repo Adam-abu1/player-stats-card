8b92b5d360e596f3feed7e89caa1e166eb625fc8
Adam-abu1 <Adam.abubakari1@gmail.com>
2021-09-27 12:43:06 +0100

    - Setting up the project with gulp js task - Added capability to use SASS for styling - Added esLint gulp task - Added server setup and LiveReload using browser-sync

---
+++ README.md
@@ -0,0 +1,21 @@
+# Frontend Development Task - Player Stats Card
+![Player Stat Card example](./assets/images/StatCard.jpg)
+### Task
+
+---
+In undertaking this task, please consider the following: 
+- Semantic and accessible mark-up 
+- CSS produced using SASS 
+- Concise Javascript solution 
+- Avoid using jQuery or an application framework 
+- NodeJS for build/task workflow 
+- Future reuse and extension of code 
+- Any documentation / notes on build
+
+
+
+## Compiling the project
+
+```
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+const { src, dest, watch, parallel, series } = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    eslint = require('gulp-eslint'),
+    browserSync = require('browser-sync').create()
+    // sassLint = require('gulp-sass-lint')
+
+/*
+ * Process the templates and convert necessary files to html then move them to build directory
+ * For the time being I am working with just html
+ */
+function generateHTML() {
+    return src('./views/*.html')
+        .pipe(dest('./build'))
+}
+
+/*
+ * Process sass files into css then add them to build directory
+ */
+function generateCSS() {
+    return src('./src/sass/*.sass')
+        .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(dest('./build/css'))
+        .pipe(browserSync.stream())
+}
+
+/*
+ * Generate and minify js files
+ */
+function generateJs() {
+    src('./js/**.js')
+        .pipe(concat())
+        .pipe(dest('./build/js'))
+}
+
+/*
+ * Compress images
+ */
+
+/*
+ * Build Spritesheet
+ */
+
+/*
+ * Watch for file changes
+ */
+function watchFiles() {
+    watch('views/**.html', generateHTML);
+    watch('sass/**.scss', generateCSS);
+    watch([ '**/*.js', '!node_modules/**'], runJSLinter);
+}
+
+/*
+ * Clean the build directory
+ */
+
+/*
+ * Run a local server, serving the build directory
+ */
+function runServer() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    })
+
+    watch('views/**.html', generateHTML);
+    watch('sass/**.scss', generateCSS);
+    watch("./public/**.html").on('change', browserSync.reload);
+}
+
+/*
+ * Refresh browser in response to changes made
+ */
+
+/*
+ * loads eslint configuration and lints code
+ */
+function runJSLinter() {
+    return src(['**/*.js','!node_modules/**'])
+        .pipe(eslint({
+            'configFile': './.eslintrc.json'
+        }))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+}
+
+/*
+ * Runs all linters available
+ */
+function runLinter() {
+    runJSLinter()
+}
+
+exports.css = generateCSS
+exports.js = generateJs
+exports.jsLint = runJSLinter
+exports.serve = runServer
+exports.html = generateHTML;
+exports.watch = watchFiles;
+exports.default = series(runLinter, parallel(generateCSS,generateHTML), runServer);
